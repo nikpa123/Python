2a583efb8ef9fd26a395d61e9df8078551b99e24
Pavel <nikpa123@yandex.ru>
2023-02-20 19:26:14 +0300

    homework_3

--- Seminar_3/task_16.py
+++ Seminar_3/task_16.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+# *Пример:*
+# 5
+#     1 2 3 4 5
+#     3
+#     -> 1
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+n = int(input("Введите размер массива: "))
+list_1 = []
+for i in range(n):
+    list_1.append(i + 1)
+print(list_1)
+x = int(input("Введите число х: "))
+count = 0
+for i in range(n):
+    if list_1[i] == x:
+        count += 1
+print(count)
