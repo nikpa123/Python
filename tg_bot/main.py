c2b00db6c19eab8a5ec0f4ef37aa271154419a94
Pavel <nikpa123@yandex.ru>
2023-03-17 17:38:34 +0300

    v2

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -9,6 +9,9 @@ if platform.system() == 'Windows':
    
 bot = Bot("5653367283:AAHd-oHcSe_ZbPMBMvAWaTy9DSlzsmxZ2Fo")
 dp = Dispatcher(bot)
+
+num1 = ''
+num2 = ''
 old_value = ''
 value = ''
 
@@ -24,38 +27,32 @@ async def com_start(message: Message):
     else:
         await message.answer(message.from_user.id, value, reply_markup=user_kb)
 
-async def calculator(message: Message):
-    global value, old_value
-    data = message.text
+# @dp.callback_query_handlers(func=lambda call: True)
+# async def calculator(query):
+#     global value, old_value
+#     data = query.text
+#
+#     if data == '_':
+#         pass
+#     elif data == 'C':
+#         value = ''
+#     elif data == '<=':
+#         if value != '':
+#             value = value[:len(value)-1]
+#     elif data == '=':
+#         try:
+#             value = str ( eval(value) )
+#         except:
+#             value = "Ошибка!"
+#     else:
+#         value += data
+#
+#     if  ( value != old_value and value != '' ) or ( '0' != old_value and value == ''):
+#         if value == '':
+#             bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text='0', reply_markup=user_kb)
+#             old_value = '0'
+#         else:
+#             bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text=value, reply_markup=user_kb)
+#     if value == "Ошибка!": value = ''
 
-    if data == '_':
-        pass
-    elif data == 'C':
-        value = ''
-    elif data == '<=':
-        if value != '':
-            value = value[:len(value)-1]
-    elif data == '=':
-        try:
-            value = str ( eval(value) )
-        except:
-            value = "Ошибка!"
-    else:
-        value += data
-
-    if  ( value != old_value and value != '' ) or ( '0' != old_value and value == ''):
-        if value == '':
-            bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text='0', reply_markup=user_kb)
-            old_value = '0'
-        else:
-            bot.edit_message_text(chat_id=query.message.chat.id, message_id=query.message.message_id, text=value, reply_markup=user_kb)
-    if value == "Ошибка!": value = ''
-
-
-
-
-
-
-
-
-executor.start_polling(dp, skip_updates=True, on_startup=on_start) # начало прослушивания (получение сообщений). skip_updates=True когда бот выключен сообщения игнорируются, skip_updates=False копятся
+executor.start_polling(dp, skip_updates=True, on_startup=on_start)
